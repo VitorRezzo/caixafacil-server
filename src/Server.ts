a36c43a5ec22dbfa7b694e77147a0899aefbf00c
VitorRezzo <cv.rezz99@gmail.com>
2024-01-17 17:33:24 -0300

    refactor: remove Resquest for the import express

--- src/Server.ts
+++ src/Server.ts
@@ -1,4 +1,4 @@
-import express,{Request,Response} from "express";
+import express,{Response} from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import http from "http";
